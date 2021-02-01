dd440a010d00ace2f4cfb6573d028cc631f7d2ef
Shaun Samoridny <ssamoridny@gmail.com>
2021-02-01 11:34:21 -0700

    Adjusted resume link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,7 +12,7 @@ import resume from '../assets/Shaun Samoridny Resume.pdf'
                 <h1 className="display-6">About Me</h1>
                 <img src={shaun} alt="Shaun Samoridny"/>
                 <p className="lead">As a life long learner I bring a desire to create, grow and always strive for more. Not content with the status quo I decided to make a massive jump, bet on myself and become a Full Stack Web Developer. In my 30+ years in the workforce, I have cultivated great personal and professional relationships where I am always doing my best to elevate those around me. I promise I will bring that same enthusiasm and dedication to your company and become an integral part of your team.</p>
-                <a href={resume}>Visit my Resume here</a>
+                <a href={resume} target="_blank" rel="noreferrer">Visit my Resume here</a>
               </div>
             </div>
             <h4>Contact me</h4>
